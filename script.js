b2d84e531e8e36f6ce8860f8661e8ea7e603b66c
aniketganorkar03 <aniketganorkar03@gmail.com>
2026-07-04 14:47:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function firstWord(s) {
-  // your code here
+  const trimmed = s.trim();
+  const spaceIndex = trimmed.indexOf(' ');
+  return spaceIndex === -1 ? trimmed : trimmed.slice(0, spaceIndex);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
